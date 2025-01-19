3d2a4811063391e327bf74be7213f21cbd2c182f
Alderj96 <geminiano_96@hotmail.com>
2025-01-19 00:48:25 -0600

    feat: add logrepository dependency

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,12 +1,15 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRespository } from '../../repository/log.repository';
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: String) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: String) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRespository: LogRespository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -18,10 +21,15 @@ export class CheckService implements CheckServiceUseCase {
         throw new Error(`Error on check service ${url}`);
       }
 
-      this.successCallback();
+      const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+      this.logRespository.saveLog(log);
+      this.successCallback?.();
       return true;
     } catch (error) {
-      this.errorCallback(`${error}`);
+      const errorMessage = `${url} is not ok. ${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+      this.logRespository.saveLog(log);
+      this.errorCallback?.(errorMessage);
       return false;
     }
   }
