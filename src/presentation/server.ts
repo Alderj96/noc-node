82eb533642e1d57879b797c4a07ab46daa473b05
Alderj96 <geminiano_96@hotmail.com>
2025-01-27 00:13:04 -0600

    feat: use email service in server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,5 @@
+import { envs } from "../config/plugins/envs.plugin";
+import { EmailService } from "../domain/email/email.service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
@@ -10,13 +12,24 @@ const fileSystemLogRepository = new LogRepositoryImpl(
 export class Server {
   public static start(): void {
     console.log('Server started!');
+    // Mandar email
+
+    const emailService = new EmailService();
+    emailService.sendEmailWithFileSystemLogs([
+      ''
+    ])
+    // emailService.sendEmail({
+    //   to: '',
+    //   subject: 'Test email',
+    //   htmlBody: '<h1>Test email</h1>',
+    // })
 
     CronService.createJob(
       '*/5 * * * * *',
       () => {
         const date = new Date();
-        // const url = `http://localhost:3000`;
-        const url = `https://www.google.com`;
+        const url = `http://localhost:3000`;
+        // const url = `https://www.google.com`;
         new CheckService(
           fileSystemLogRepository,
           () => console.log(`${url} is working!`),
