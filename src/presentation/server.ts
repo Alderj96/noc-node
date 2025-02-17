8262fd8a55cc1f5e703ac7c786a9d2ad69d7d816
Alderj96 <geminiano_96@hotmail.com>
2025-02-17 00:49:12 -0600

    feat: add new file to send email logs

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,7 @@
 import { envs } from "../config/plugins/envs.plugin";
-import { EmailService } from "../domain/email/email.service";
+import { EmailService } from '../domain/email/email.service';
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
@@ -9,33 +10,38 @@ const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
 );
 
+const emailService = new EmailService();
+
 export class Server {
   public static start(): void {
     console.log('Server started!');
     // Mandar email
 
-    const emailService = new EmailService();
-    emailService.sendEmailWithFileSystemLogs([
-      ''
-    ])
+    // new SendEmailLogs(
+    //   emailService,
+    //   fileSystemLogRepository
+    // ).execute([
+    // ])
+    // emailService.sendEmailWithFileSystemLogs([
+    // ])
     // emailService.sendEmail({
-    //   to: '',
+    //   to: 'geminiano_96@hotmail.com',
     //   subject: 'Test email',
     //   htmlBody: '<h1>Test email</h1>',
     // })
 
-    CronService.createJob(
-      '*/5 * * * * *',
-      () => {
-        const date = new Date();
-        const url = `http://localhost:3000`;
-        // const url = `https://www.google.com`;
-        new CheckService(
-          fileSystemLogRepository,
-          () => console.log(`${url} is working!`),
-          console.error,
-        ).execute(url);
-      }
-    );
+    // CronService.createJob(
+    //   '*/5 * * * * *',
+    //   () => {
+    //     const date = new Date();
+    //     const url = `http://localhost:3000`;
+    //     // const url = `https://www.google.com`;
+    //     new CheckService(
+    //       fileSystemLogRepository,
+    //       () => console.log(`${url} is working!`),
+    //       console.error,
+    //     ).execute(url);
+    //   }
+    // );
   }
 }
