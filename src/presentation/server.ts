fd43825edca53d126cf6c65894ad141dddcbb938
Alderj96 <geminiano_96@hotmail.com>
2025-03-01 15:37:30 -0600

    feat: use mongo datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,19 +1,22 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { EmailService } from '../domain/email/email.service';
+import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource()
+  // new FileSystemDatasource()
+  new MongoLogDatasource()
 );
 
 const emailService = new EmailService();
 
 export class Server {
-  public static start(): void {
+  public static async start() {
     console.log('Server started!');
     // Mandar email
 
@@ -29,6 +32,9 @@ export class Server {
     //   subject: 'Test email',
     //   htmlBody: '<h1>Test email</h1>',
     // })
+
+    const logs = await fileSystemLogRepository.getLogs(LogSeverityLevel.high);
+    console.log(logs)
 
     // CronService.createJob(
     //   '*/5 * * * * *',
