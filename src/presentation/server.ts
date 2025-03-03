41c8df80fe1777d07ca502075fdd8c4105592bff
Alderj96 <geminiano_96@hotmail.com>
2025-03-03 01:19:21 -0600

    feat: add CheckServiceMultiple

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,15 +2,22 @@ import { envs } from "../config/plugins/envs.plugin";
 import { EmailService } from '../domain/email/email.service';
 import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgreLogDatasource } from "../infrastructure/datasources/postgre-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  // new FileSystemDatasource()
+const fsLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
+const mongoLogRepository = new LogRepositoryImpl(
   new MongoLogDatasource()
+);
+const postgreLogRepository = new LogRepositoryImpl(
+  new PostgreLogDatasource()
 );
 
 const emailService = new EmailService();
@@ -33,21 +40,30 @@ export class Server {
     //   htmlBody: '<h1>Test email</h1>',
     // })
 
-    const logs = await fileSystemLogRepository.getLogs(LogSeverityLevel.high);
-    console.log(logs)
+    // const logs = await fileSystemLogRepository.getLogs(LogSeverityLevel.high);
+    // console.log(logs)
 
-    // CronService.createJob(
-    //   '*/5 * * * * *',
-    //   () => {
-    //     const date = new Date();
-    //     const url = `http://localhost:3000`;
-    //     // const url = `https://www.google.com`;
-    //     new CheckService(
-    //       fileSystemLogRepository,
-    //       () => console.log(`${url} is working!`),
-    //       console.error,
-    //     ).execute(url);
-    //   }
-    // );
+    CronService.createJob(
+      '*/5 * * * * *',
+      () => {
+        const date = new Date();
+        const url = `http://localhost:3000`;
+        // const url = `https://www.google.com`;
+        // new CheckService(
+        //   fileSystemLogRepository,
+        //   () => console.log(`${url} is working!`),
+        //   console.error,
+        // ).execute(url);
+        new CheckServiceMultiple(
+          [
+            fsLogRepository,
+            mongoLogRepository,
+            postgreLogRepository,
+          ],
+          () => console.log(`${url} is working!`),
+          console.error,
+        ).execute(url);
+      }
+    );
   }
 }
